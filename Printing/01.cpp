c479a9f6159a465f9e46fbeb12bb7281a18441aa
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-11-13 20:58:54 +0200

    Create 01.cpp

---
+++ Printing/01.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"I am Abdeltawab";
+	return 0;
+
+	cout<<endl;
+	cout<<"I was born in Giza\n";
+	cout<<"I student at Must";
+	cout<<endl;
+}
